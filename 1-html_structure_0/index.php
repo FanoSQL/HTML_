b029de12dba655ccb99e4c024e122b3684adfc71
Go NUMERIQUE <decs@datatech07.onmicrosoft.com>
2025-11-10 12:02:22 +0100

    Premier exercice struc html 0

---
+++ 1-html_structure_0/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Titre de ma page</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+</head>
+
+<body>
+
+<div id="main">
+
+<header>
+
+<h1>Mon titre</h1>
+
+</header>
+
+<section>
+
+</section>
+
+<footer>
+
+<p>texte du footer</p>
+
+</footer>
+
+</div>
+
+</body>
+
+</html>
